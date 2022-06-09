5ec874d90410366d506f22c4b5427985831e8964
Bas van Stein <bas9112@gmail.com>
2022-06-09 09:02:20 +0200

    changed plots

--- paper experiments/plot.py
+++ paper experiments/plot.py
@@ -23,13 +23,13 @@ def plotSensitivity(f, d):
     title=f"Average Sensitivity Scores per Sample Size on F{f} D{d}"
     #print(sens.shape, conf.shape) #3, 10, 5, 2 = sample_sizes, reps, algs, dim
 
-    x_samples = np.load(f"samples{filename}.npy")
-    print(x_samples)
-    sens = np.load(f"sens{filename}.npy")
-    conf = np.load(f"conf{filename}.npy")
+    x_samples = np.load(f"npy/samples{filename}.npy")
+    sens = np.load(f"npy/sens{filename}.npy")
+    conf = np.load(f"npy/conf{filename}.npy")
 
     avg_sens = np.mean(sens, axis=1)
     avg_samples = np.mean(x_samples, axis=1)
+    print(avg_samples.shape)
     avg_conf = np.mean(conf, axis=1)
     std_sens = np.std(sens, axis=1)
 
@@ -37,31 +37,28 @@ def plotSensitivity(f, d):
     LINE_STYLES = ['solid', 'dashed', 'dashdot', 'dotted']
     NUM_STYLES = len(LINE_STYLES)
     colors = sns.color_palette('husl', n_colors=avg_sens.shape[2])
-    labels = ['Morris','Sobol','Fast', "RDB-Fast", "Delta", "DGSM", "R2", "Pearson", "RF", "Linear"]
+    labels = ['Morris','Sobol','Fast', "RDB-Fast", "Delta", "DGSM", "Pawn", "Pearson", "RF", "Linear"]
     cols = labels
     rows = ['X{}'.format(row) for row in range(avg_sens.shape[2])]
 
-    fig, axes = plt.subplots(3, 3, sharey=True, figsize=[16,10])
+    fig, axes = plt.subplots(2, 5, sharey=True, figsize=[16,6])
     fig.suptitle(title)
     
     i_correct = 0
     for j in np.arange(avg_sens.shape[2]):
         for i in np.arange(avg_sens.shape[1]):
-            if (labels[i] == "R2"):
-                i_correct = 1
-                continue
-            ind = i - i_correct
-            axes[int(ind/3),ind%3].fill_between(avg_samples[:,i,j], (avg_sens[:,i,j]-std_sens[:,i,j]), (avg_sens[:,i,j]+std_sens[:,i,j]), color=colors[j], alpha=0.2 )
-            axes[int(ind/3),ind%3].fill_between(avg_samples[:,i,j], (avg_sens[:,i,j]-avg_conf[:,i,j]), (avg_sens[:,i,j]+avg_conf[:,i,j]), color=colors[j], alpha=0.1 )
-            axes[int(ind/3),ind%3].plot(avg_samples[:,i,j],avg_sens[:,i,j],color=colors[j], linestyle=LINE_STYLES[j%NUM_STYLES] , label = 'X'+str(j))
-            axes[int(ind/3),ind%3].set_xticks([128,256,512,1024,2048,4096,8192,16384,32768])
-            axes[int(ind/3),ind%3].set_xscale('log', base=2)
-            axes[int(ind/3),ind%3].set_ylim([0.0,1.0])
-            axes[int(ind/3),ind%3].set_title(labels[i])
-            if (ind/3 >= 2):
-                axes[int(ind/3),ind%3].set_xlabel("samples")
-            if (ind%3 == 0):
-                axes[int(ind/3),ind%3].set_ylabel("normalized sensitivity")
+            ind = i
+            axes[int(ind/5),ind%5].fill_between(avg_samples[:,i], (avg_sens[:,i,j]-std_sens[:,i,j]), (avg_sens[:,i,j]+std_sens[:,i,j]), color=colors[j], alpha=0.2 )
+            #axes[int(ind/5),ind%5].fill_between(x_samples, (avg_sens[:,i,j]-avg_conf[:,i,j]), (avg_sens[:,i,j]+avg_conf[:,i,j]), color=colors[j], alpha=0.1 )
+            axes[int(ind/5),ind%5].plot(avg_samples[:,i],avg_sens[:,i,j],color=colors[j], linestyle=LINE_STYLES[j%NUM_STYLES] , label = 'X'+str(j))
+            axes[int(ind/5),ind%5].set_xticks([128,256,512,1024,2048,4096,8192,16384,32768])
+            axes[int(ind/5),ind%5].set_xscale('log', base=2)
+            axes[int(ind/5),ind%5].set_ylim([0.0,1.0])
+            axes[int(ind/5),ind%5].set_title(labels[i])
+            if (ind/5 >= 2):
+                axes[int(ind/5),ind%5].set_xlabel("samples")
+            if (ind%5 == 0):
+                axes[int(ind/5),ind%5].set_ylabel("normalized sensitivity")
 
     lines_labels = [ax.get_legend_handles_labels() for ax in [axes[0,0]]]
     lines, labels = [sum(lol, []) for lol in zip(*lines_labels)]
@@ -70,16 +67,17 @@ def plotSensitivity(f, d):
     fig.legend(lines, labels)
     fig.tight_layout()
     plt.tight_layout()
-    plt.savefig(f"img/{filename}.pdf")
+    plt.savefig(f"img/{filename}.png")
     #plt.show()
     plt.clf()
     
-plotSensitivity(1,2)
+#plotSensitivity(2,2)
 
 from benchmark import bbobbenchmarks as bn
 
 fIDs = bn.nfreeIDs[:]    # for all fcts
 
-for dim in [2,5,10,20,100]:
-    for f in tqdm(fIDs, position=0):
-        plotSensitivity(f,dim)
+plotSensitivity(2,8)
+#for dim in [2,5,10,20,100]:
+#    for f in tqdm(fIDs, position=0):
+#        plotSensitivity(f,dim)
