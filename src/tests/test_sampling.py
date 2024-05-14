1578879a2c55b4beb7cc0ad9ef1d353e8bb426c9
Bas van Stein <bas9112@gmail.com>
2024-05-14 21:26:21 +0200

    another test

--- src/tests/test_sampling.py
+++ src/tests/test_sampling.py
@@ -54,5 +54,5 @@ def test_generate_samples_with_model():
         problem["num_vars"] + 1
     ), f"Unexpected number of samples for Morris {len(report.x_morris)}"
     assert len(report.x_sobol) == 128 * (
-        2 * problem["num_vars"] + 2
+        problem["num_vars"] + 2
     ), f"Unexpected number of samples for Sobol {len(report.x_sobol)} != {128*(2*problem['num_vars']+2)}"
